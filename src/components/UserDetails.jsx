ed6e63e23e4740cbf249e619aaa4eabde4460033
Giovanni Sardisco <g.sardisco@betacom.it>
2026-07-19 22:59:46 +0200

    Display complete user details page
    Render the selected user's profile image and personal details, including
    ID, name, surname, age, description, profession, and quote. Skip the
    details view when no user is logged in.

--- src/components/UserDetails.jsx
+++ src/components/UserDetails.jsx
@@ -1,11 +1,31 @@
 import React from 'react'
 import { useLocation, useParams } from 'react-router-dom';
+import MyContext from './Context';
 
 function UserDetails() {
+  const { loggedUser } = React.useContext(MyContext)
+  if (!loggedUser.name) {
+    return
+  }
+
+  const baseImgURL = 'https://www.ugobetori.it/_notes/api-test/unauth/img/';
+
   const { id } = useParams()
   const { state: user } = useLocation()
+
   return (
-    <div>UserDetails {user?.name} {id}</div>
+    <>
+      <h2>UserDetails</h2>
+
+      <img src={baseImgURL + user.id + ".png"} />
+      <p>Id: {id}</p>
+      <p>Name: {user.name}</p>
+      <p>Surname: {user.surname}</p>
+      <p>Age: {user.age}</p>
+      <p>Description: {user.description}</p>
+      <p>Profession: {user.profession}</p>
+      <p>Quote: {user.quote}</p>
+    </>
   )
 }
 
